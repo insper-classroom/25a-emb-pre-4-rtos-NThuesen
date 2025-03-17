f26741408d01219b4bf04083c06eb7cf4da73f69
NThuesen <nicholasnt@al.insper.edu.br>
2025-03-17 08:01:22 -0300

    ex 4

--- exe4/main.c
+++ exe4/main.c
@@ -14,11 +14,27 @@ const int LED_PIN_G = 6;
 
 QueueHandle_t xQueueButId;
 SemaphoreHandle_t xSemaphore_r;
+QueueHandle_t xQueueButIdG;
+SemaphoreHandle_t xSemaphore_g;
 
 void btn_callback(uint gpio, uint32_t events) {
-    if (events == 0x4) { // fall edge
-        xSemaphoreGiveFromISR(xSemaphore_r, 0);
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+    int btn_id = 0;
+
+    if (events & GPIO_IRQ_EDGE_FALL) { // Botão pressionado
+        if (gpio == BTN_PIN_R) {
+            btn_id = 1;
+            xSemaphoreGiveFromISR(xSemaphore_r, &xHigherPriorityTaskWoken);
+        } else if (gpio == BTN_PIN_G) {
+            btn_id = 2;
+            xSemaphoreGiveFromISR(xSemaphore_g, &xHigherPriorityTaskWoken);
+        }
+        
+        // Enviar o ID do botão pressionado para a fila
+        xQueueSendFromISR(xQueueButId, &btn_id, &xHigherPriorityTaskWoken);
     }
+
+    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
 }
 
 void led_1_task(void *p) {
@@ -62,15 +78,61 @@ void btn_1_task(void *p) {
     }
 }
 
+void led_2_task(void *p) {
+    gpio_init(LED_PIN_G);
+    gpio_set_dir(LED_PIN_G, GPIO_OUT);
+
+    int delay = 0;
+
+    while (true) {
+        if (xQueueReceive(xQueueButIdG, &delay, 0)) {
+            printf("%d\n", delay);
+        }
+
+        if (delay > 0) {
+            gpio_put(LED_PIN_G, 1);
+            vTaskDelay(pdMS_TO_TICKS(delay));
+            gpio_put(LED_PIN_G, 0);
+            vTaskDelay(pdMS_TO_TICKS(delay));
+        }
+    }
+}
+
+void btn_2_task(void *p) {
+    gpio_init(BTN_PIN_G);
+    gpio_set_dir(BTN_PIN_G, GPIO_IN);
+    gpio_pull_up(BTN_PIN_G);
+    gpio_set_irq_enabled_with_callback(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true,
+                                       &btn_callback);
+
+    int delay = 0;
+    while (true) {
+        if (xSemaphoreTake(xSemaphore_g, pdMS_TO_TICKS(500)) == pdTRUE) {
+            if (delay < 1000) {
+                delay += 100;
+            } else {
+                delay = 100;
+            }
+            printf("delay btn %d \n", delay);
+            xQueueSend(xQueueButIdG, &delay, 0);
+        }
+    }
+}
+
 int main() {
     stdio_init_all();
     printf("Start RTOS \n");
 
     xQueueButId = xQueueCreate(32, sizeof(int));
     xSemaphore_r = xSemaphoreCreateBinary();
+    xQueueButIdG = xQueueCreate(32, sizeof(int));
+    xSemaphore_g = xSemaphoreCreateBinary();
 
     xTaskCreate(led_1_task, "LED_Task 1", 256, NULL, 1, NULL);
     xTaskCreate(btn_1_task, "BTN_Task 1", 256, NULL, 1, NULL);
+
+    xTaskCreate(led_2_task, "LED_Task 2", 256, NULL, 1, NULL);
+    xTaskCreate(btn_2_task, "BTN_Task 2", 256, NULL, 1, NULL);
 
     vTaskStartScheduler();
 
